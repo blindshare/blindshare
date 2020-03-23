e56ed2fbe79b0c985c2b245a026b928bc3b3b9a4
hydra44 <may24x@yahoo.com>
2020-03-23 16:43:12 +0100

    not working version

--- server.py
+++ server.py
@@ -10,18 +10,49 @@ class BlindShare(object):
     @cherrypy.expose
     def index(self):
         a = []
-        a.append("<HTML><TITLE>Blind Share</TITLE>")
-        a.append("<BODY>")
-        a.append("<H1>Blind Share - ver 0.4</H1>")
-        a.append("<HR>")
+        a.append("<!DOCTYPE html> \n")
+        a.append("<HTML><TITLE>Blind Share</TITLE>\n")
+        a.append("<BODY>\n")
+        a.append("<H1>Blind Share - ver 0.5</H1>\n")
+        a.append("<HR>\n")
         headers = cherrypy.request.headers
         print(headers)
         ClientCertSha1Fingerprint = headers.get('X-Ssl-Cert')
-        a.append("Clients Cert sha1 Fingerprint: ")
+        a.append("Clients Cert sha1 Fingerprint: \n")
         a.append(ClientCertSha1Fingerprint)
-        a.append("<HR>")
-        a.append("<form action=\"getHash\" method=\"GET\">Please insert hash: <input type=\"text\" name=\"item\" /><input value=\"get\" type=\"submit\" /></form>")
+        a.append("<HR>\n")
+        a.append("<form action=\"getHash\" method=\"GET\">Please insert hash: <input type=\"text\" name=\"item\" /><input value=\"get\" type=\"submit\" /></form>\n")
+        a.append("</BODY></HTML>\n")
+
+        with sqlite3.connect(cherrypy.request.app.config['cfg']['db']) as con:
+            fItems = con.execute("SELECT eID=? AND userID=?", [fileID, userID])
+
         return a
+
+    @cherrypy.expose
+    def index2(self):
+        a = []
+        a.append("<!DOCTYPE html> \n")
+        a.append("<HTML><TITLE>Blind Share</TITLE>\n")
+        a.append("<BODY>\n")
+        a.append("<H1>Blind Share - ver 0.5</H1>\n")
+        a.append("<HR>\n")
+        headers = cherrypy.request.headers
+        print(headers)
+        ClientCertSha1Fingerprint = headers.get('X-Ssl-Cert')
+        a.append("Clients Cert sha1 Fingerprint: \n")
+        a.append(ClientCertSha1Fingerprint)
+        a.append("<HR>\n")
+        a.append("This is a private Content Management Service site NOT a one-click hoster<P>\n")
+        a.append("If you see this text, access to this site has not been granted or any sharing of public content has been disabled.\n")
+        a.append("</BODY></HTML>\n")
+        return a
+
+
+    @cherrypy.expose
+    def getID(self, ClientCertSha1Fingerprint=""):
+
+
 
     @cherrypy.expose
     def getHash(self, item=None):
